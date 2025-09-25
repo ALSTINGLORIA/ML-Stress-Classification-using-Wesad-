813220e21464384896994cc25adabf7601b43723
alstingloria <alstingloria@gmail.com>
2025-09-25 13:41:22 +0530

    added undersampler

--- ext.py
+++ ext.py
@@ -3,8 +3,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, accuracy_score
+from imblearn.under_sampling import RandomUnderSampler
+#from imblearn.over_sampling import SMOTE
 
-pickle_file_path = r"your file name here.pkl"
+pickle_file_path = r"file path.pkl"
 
 
 data = pd.read_pickle(pickle_file_path)
@@ -33,29 +35,29 @@ ch_df = pd.DataFrame(ch_array, columns = Columns)
 stress_data = ch_df[ch_df['label'].isin([1, 2, 3, 4])]
 
 
-ecg_25th = np.percentile(ch_df[ch_df['label'] == 2]['cecg'], 25)
-ecg_75th = np.percentile(ch_df[ch_df['label'] == 2]['cecg'], 75)
+ecg_30th = np.percentile(ch_df[ch_df['label'] == 2]['cecg'], 30)
+ecg_60th = np.percentile(ch_df[ch_df['label'] == 2]['cecg'], 60)
 
-emg_25th = np.percentile(ch_df[ch_df['label'] == 2]['cemg'], 25)
-emg_75th = np.percentile(ch_df[ch_df['label'] == 2]['cemg'], 75)
+emg_30th = np.percentile(ch_df[ch_df['label'] == 2]['cemg'], 30)
+emg_60th = np.percentile(ch_df[ch_df['label'] == 2]['cemg'], 60)
 
-eda_25th = np.percentile(ch_df[ch_df['label'] == 2]['ceda'], 25)
-eda_75th = np.percentile(ch_df[ch_df['label'] == 2]['ceda'], 75)
+eda_30th = np.percentile(ch_df[ch_df['label'] == 2]['ceda'], 30)
+eda_60th = np.percentile(ch_df[ch_df['label'] == 2]['ceda'], 60)
 
-temp_25th = np.percentile(ch_df[ch_df['label'] == 2]['ctemp'], 25)
-temp_75th = np.percentile(ch_df[ch_df['label'] == 2]['ctemp'], 75)
+temp_30th = np.percentile(ch_df[ch_df['label'] == 2]['ctemp'], 30)
+temp_60th = np.percentile(ch_df[ch_df['label'] == 2]['ctemp'], 60)
 
-resp_25th = np.percentile(ch_df[ch_df['label'] == 2]['cresp'], 25)
-resp_75th = np.percentile(ch_df[ch_df['label'] == 2]['cresp'], 75)
+resp_30th = np.percentile(ch_df[ch_df['label'] == 2]['cresp'], 30)
+resp_60th = np.percentile(ch_df[ch_df['label'] == 2]['cresp'], 60)
 
-cax_25th = np.percentile(ch_df[ch_df['label'] == 2]['cax'], 25)
-cax_75th = np.percentile(ch_df[ch_df['label'] == 2]['cax'], 75)
+cax_30th = np.percentile(ch_df[ch_df['label'] == 2]['cax'], 30)
+cax_60th = np.percentile(ch_df[ch_df['label'] == 2]['cax'], 60)
 
-cay_25th = np.percentile(ch_df[ch_df['label'] == 2]['cay'], 25)
-cay_75th = np.percentile(ch_df[ch_df['label'] == 2]['cay'], 75)
+cay_30th = np.percentile(ch_df[ch_df['label'] == 2]['cay'], 30)
+cay_60th = np.percentile(ch_df[ch_df['label'] == 2]['cay'], 60)
 
-caz_25th = np.percentile(ch_df[ch_df['label'] == 2]['caz'], 25)
-caz_75th = np.percentile(ch_df[ch_df['label'] == 2]['caz'], 75)
+caz_30th = np.percentile(ch_df[ch_df['label'] == 2]['caz'], 30)
+caz_60th = np.percentile(ch_df[ch_df['label'] == 2]['caz'], 60)
 
 classified_data = stress_data.copy()
 classified_data['label'] = classified_data['label'].replace({1: 0, 3: 0, 4: 0})
@@ -75,8 +77,8 @@ def categorize_and_classify(data, features, percentiles):
         temp_categories = []
 
         for feature in features:
-            lower_percentile = percentiles[f"{feature}_25th"]
-            upper_percentile = percentiles[f"{feature}_75th"]
+            lower_percentile = percentiles[f"{feature}_30th"]
+            upper_percentile = percentiles[f"{feature}_60th"]
 
             value = row[feature]
 
@@ -93,30 +95,49 @@ def categorize_and_classify(data, features, percentiles):
 
 features = ['cecg', 'cemg', 'ceda', 'ctemp', 'cresp', 'cax', 'cay', 'caz']
 percentiles = {
-    'cecg_25th': ecg_25th, 'cecg_75th': ecg_75th,
-    'cemg_25th': emg_25th, 'cemg_75th': emg_75th,
-    'ceda_25th': eda_25th, 'ceda_75th': eda_75th,
-    'ctemp_25th': temp_25th, 'ctemp_75th': temp_75th,
-    'cresp_25th': resp_25th, 'cresp_75th': resp_75th,
-    'cax_25th': cax_25th, 'cax_75th': cax_75th,
-    'cay_25th': cay_25th, 'cay_75th': cay_75th,
-    'caz_25th': caz_25th, 'caz_75th': caz_75th
+    'cecg_30th': ecg_30th, 'cecg_60th': ecg_60th,
+    'cemg_30th': emg_30th, 'cemg_60th': emg_60th,
+    'ceda_30th': eda_30th, 'ceda_60th': eda_60th,
+    'ctemp_30th': temp_30th, 'ctemp_60th': temp_60th,
+    'cresp_30th': resp_30th, 'cresp_60th': resp_60th,
+    'cax_30th': cax_30th, 'cax_60th': cax_60th,
+    'cay_30th': cay_30th, 'cay_60th': cay_60th,
+    'caz_30th': caz_30th, 'caz_60th': caz_60th
 }
 
 
 classified_data = categorize_and_classify(classified_data, features, percentiles)
 
 
+label_counts = classified_data['label'].value_counts()
+print("Number of entries for each label:")
+print(label_counts)
+
+label_0_count = label_counts.get(0, 0)  
+label_1_count = label_counts.get(1, 0)
+label_2_count = label_counts.get(2, 0)
+label_3_count = label_counts.get(3, 0)
+
+print(f"Label 0: {label_0_count} entries")
+print(f"Label 1: {label_1_count} entries")
+print(f"Label 2: {label_2_count} entries")
+print(f"Label 3: {label_3_count} entries")
+
 
 X = classified_data[['cecg', 'cemg', 'ceda', 'ctemp', 'cresp', 'cax', 'cay', 'caz']] 
 y = classified_data['label'] 
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
-model = RandomForestClassifier(n_estimators=100, random_state=42)
-model.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
+"""
+smote = SMOTE(random_state=42)
+X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
+"""
+undersampler = RandomUnderSampler(sampling_strategy='auto', random_state=42)
+X_train_resampled, y_train_resampled = undersampler.fit_resample(X_train, y_train)
+model = RandomForestClassifier(n_estimators=20, random_state=42,class_weight='balanced')
+model.fit(X_train_resampled, y_train_resampled)
+# model.fit(X_train, y_train)
 
 
 y_pred = model.predict(X_test)
